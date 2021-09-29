59e37c5e16d6b2599e2e824f4e91d597d476729d
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-09-29 09:34:42 -0400

    migrated review table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_133131) do
+ActiveRecord::Schema.define(version: 2021_09_29_133315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,17 @@ ActiveRecord::Schema.define(version: 2021_09_29_133131) do
     t.index ["director_id"], name: "index_movies_on_director_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "rating"
+    t.bigint "movie_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -48,4 +59,6 @@ ActiveRecord::Schema.define(version: 2021_09_29_133131) do
   end
 
   add_foreign_key "movies", "directors"
+  add_foreign_key "reviews", "movies"
+  add_foreign_key "reviews", "users"
 end
