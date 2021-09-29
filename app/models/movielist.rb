d04aa4f3ac44a6156491a607ca1e9da6900a2353
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-09-29 14:04:00 -0400

    added validations to movielist, review and user model

--- app/models/movielist.rb
+++ app/models/movielist.rb
@@ -1,3 +1,7 @@
 class Movielist < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence:true, uniqueness: true
+  validates :description, presence:true
+  validates :image, presence:true, uniqueness:true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
  has_many :reviews, dependent: :destroy
  has_many :movies, through: :reviews
  has_many :movielist, dependent: :destroy
+
+ validates :username, presence: true, uniqueness: true
+ validates :email, presence: true, uniqueness: true
+ validates :password_digest, presence: true
 end
