44fc63bfcb46c6022e07b29a64790f8acadbb8e5
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-09-29 13:59:31 -0400

    added validations to director and movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,14 @@
 class Movie < ApplicationRecord
-   belongs_to :director
+  belongs_to :director
   has_many :reviews, dependent: :destroy
   has_many :students, through: :reviews
+
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :language, presence: true
+  validates :runtime, presence: true
+  validates :rating, presence: true
+  validates :image, presence: true
+  validates :video_url, presence: true
+  validates :genre, presence: true
 end
